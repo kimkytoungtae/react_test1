c8362790c60f9c7cf9883006b3023c720430133e
kimkytoungtae <kind4209@gmail.com>
2025-10-01 09:31:36 +0900

    ..

---
+++ src/Pages/Memo.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+export default function Memo() {
+  return (
+    <div className="content-margin-padding">
+      <div>홈 화면</div>
+    </div>
+  );
+}
