d55d0b6faff4e5c363c5ef4ee7f33690fca3b613
kimkytoungtae <kind4209@gmail.com>
2025-10-01 10:02:22 +0900

    ..

--- src/Pages/Memo.tsx
+++ src/Pages/Memo.tsx
@@ -9,6 +9,7 @@ export default function Memo() {
           className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border-2 border-black focus:ring-black focus:border-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-black dark:focus:border-black"
           placeholder="메모를 입력하세요"
           cols={100}
+          rows={20}
         />
       </div>
     </div>
