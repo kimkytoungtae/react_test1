305b847ecb812ea09f368c30ba62a842de1f08b3
kimkytoungtae <kind4209@gmail.com>
2025-10-17 10:43:49 +0900

    ....

--- src/Pages/MemoList.tsx
+++ src/Pages/MemoList.tsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react";
+import "./MemoList.css";
+
+interface Memotype {
+  content: string;
+  createdDt: string;
+  updatedDt: string;
+  id: number;
+  title: string;
+}
 
 export default function MemoList() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const [memo, setMemo] = useState<Memotype[]>([]);
+
   useEffect(() => {
     getMemoList();
   }, []);
+
   async function getMemoList() {
     const fetchOption = {
       method: "GET",
       headers: {
         Authorization: "",
       },
-      body: JSON.stringify(""),
     };
-    let res = await fetch(
+    let res: any = await fetch(
       `${API_BASE_URL}/api/board/get_memo_list`,
       fetchOption
     );
+    res = await res.json();
+    setMemo(res?.data ?? []);
     console.log(`## res: `, res);
   }
 
